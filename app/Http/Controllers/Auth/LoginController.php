c608eb6dc5c42cfebcb07db0a2053bb6837ac6c3
Edmir Lôbo <ejuniorls@outlook.com>
2025-11-22 17:36:30 -0300

    validação de formulário

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -16,6 +16,11 @@ class LoginController extends Controller
 
     public function login()
     {
+        request()->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
         if ($user = User::query()
             ->where('email', request()->email)
             ->first()) {
@@ -27,6 +32,6 @@ class LoginController extends Controller
             }
         }
 
-        return back()->with(["messagem" => "não encontrado"]);
+        return back()->with(["mensagem" => "não encontrado"]);
     }
 }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,12 +1,26 @@
 <div>
     <h1>Login</h1>
 
+    @if($mensagem = session()->get('mensagem'))
+        <div>{{ $mensagem }}</div>
+    @endif
+
     <form action="/login" method="post">
 
         @csrf
+        <div>
+            <input type="text" name="email" id="email" placeholder="E-mail" value="{{ old('email') }}">
+            @error('email')
+            <span>{{ $message }}</span>
+            @enderror
+        </div>
 
-        <input type="email" name="email" id="email" placeholder="E-mail" value="{{ old('email') }}">
-        <input type="password" name="password" id="password" placeholder="Senha">
+        <div>
+            <input type="password" name="password" id="password" placeholder="Senha">
+            @error('password')
+            <span>{{ $message }}</span>
+            @enderror
+        </div>
 
         <button>Logar</button>
     </form>
