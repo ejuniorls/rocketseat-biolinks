06d35b7649ee08fd953b20cbca6105835f7df7d7
Edmir Lôbo <ejuniorls@outlook.com>
2025-12-02 15:47:37 -0300

    ordenando-os-links

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,8 @@
 <div>
     <h1>Dashboard</h1>
 
+    <h2>User {{ auth()->user()->name }} :: {{ auth()->id() }}</h2>
+
     @if($message = session()->get('message'))
         <div>{{ $message }}</div>
     @endif
@@ -10,7 +12,10 @@
     <ul>
         @foreach($links as $link)
             <li>
-                <a href="{{ route('links.edit', $link) }}">{{ $link->name }}</a>
+                <a href="{{ route('links.edit', $link) }}">
+                    {{ $link->id }} . {{ $link->name }}
+                </a>
+
                 <form action="{{ route('links.destroy', $link) }}" method="post" onsubmit="return confirm('are you sure?')">
                     @csrf
                     @method('DELETE')
