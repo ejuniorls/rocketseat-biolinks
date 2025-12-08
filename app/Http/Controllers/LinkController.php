53a1ca0b50ad61adf89a2f72764849ebe1992ab5
Edmir Lôbo <ejuniorls@outlook.com>
2025-12-08 19:41:35 -0300

    refatorando-ordenacao-para-dentro-do-model

--- app/Http/Controllers/LinkController.php
+++ app/Http/Controllers/LinkController.php
@@ -88,36 +88,14 @@ class LinkController extends Controller
 
     public function up(Link $link)
     {
-        $order = $link->sort;
-        $newOrder = $order - 1;
-
-        /** @var User $user */
-        $user = auth()->user();
-
-        $swapWith = $user->links()
-            ->where('sort', '=', $newOrder)
-            ->first();
-
-        $link->fill(['sort' => $newOrder])->save();
-        $swapWith->fill(['sort' => $order])->save();
+        $link->moveUp();
 
         return to_route('dashboard')->with('message', 'Link updated successfully');
     }
 
     public function down(Link $link)
     {
-        $order = $link->sort;
-        $newOrder = $order + 1;
-
-        /** @var User $user */
-        $user = auth()->user();
-
-        $swapWith = $user->links()
-            ->where('sort', '=', $newOrder)
-            ->first();
-
-        $link->fill(['sort' => $newOrder])->save();
-        $swapWith->fill(['sort' => $order])->save();
+        $link->moveDown();
 
         return to_route('dashboard')->with('message', 'Link updated successfully');
     }
--- app/Models/Link.php
+++ app/Models/Link.php
@@ -9,4 +9,39 @@ class Link extends Model
 {
     /** @use HasFactory<\Database\Factories\LinkFactory> */
     use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Function responsible for sorting link position up or down
+     * @param int $to +1 for moving down and -1 for moving up
+     * @return void
+     */
+    private function move($to)
+    {
+        $order = $this->sort;
+        $newOrder = $order + $to;
+
+        $user = $this->user;
+
+        $swapWith = $user->links()
+            ->where('sort', '=', $newOrder)
+            ->first();
+
+        $this->fill(['sort' => $newOrder])->save();
+        $swapWith->fill(['sort' => $order])->save();
+    }
+
+    public function moveUp()
+    {
+        $this->move(-1);
+    }
+
+    public function moveDown()
+    {
+        $this->move(+1);
+    }
 }
