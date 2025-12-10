718702707ba22fd8d0982bbeebbd358855a6633b
Edmir Lôbo <ejuniorls@outlook.com>
2025-12-10 13:45:54 -0300

    atualizando-o-profile

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,57 @@
+<div>
+
+    <h1>Profile</h1>
+
+    @if($mensagem = session()->get('mensagem'))
+        <div>{{ $mensagem }}</div>
+    @endif
+
+    <form action="{{ route('profile') }}" method="post">
+
+        @csrf
+        @method('PUT')
+
+{{--        <div>--}}
+{{--            <input type="text" name="photo" id="photo" placeholder="Photo" value="{{ old('photo') }}">--}}
+{{--            @error('photo')--}}
+{{--            <span>{{ $message }}</span>--}}
+{{--            @enderror--}}
+{{--        </div>--}}
+
+{{--        <br>--}}
+
+        <div>
+            <input type="text" name="name" id="name" placeholder="Name" value="{{ old('name', $user->name) }}">
+            @error('name')
+            <span>{{ $message }}</span>
+            @enderror
+        </div>
+
+        <br>
+
+        <div>
+            <textarea name="description" id="description" placeholder="Description">
+                {{ old('description', $user->description) }}
+            </textarea>
+            @error('description')
+            <span>{{ $message }}</span>
+            @enderror
+        </div>
+
+        <br>
+
+        <div>
+            <span>biolinks.com.br/</span>
+            <input type="text" name="handler" id="handler" placeholder="@seulink" value="{{ old('handler', $user->handler) }}">
+            @error('handler')
+            <span>{{ $message }}</span>
+            @enderror
+        </div>
+
+        <br>
+
+        <a href="{{ route('dashboard') }}">Cancel</a>
+
+        <button>Update</button>
+    </form>
+</div>
