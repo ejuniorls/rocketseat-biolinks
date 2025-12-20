b8c083ad8209f93fe61c44944b98a1f9f263fdbb
Edmir Lôbo <ejuniorls@outlook.com>
2025-12-20 17:25:47 -0300

    prettier create e edit link

--- resources/views/links/edit.blade.php
+++ resources/views/links/edit.blade.php
@@ -1,18 +1,18 @@
 <x-layout.app>
-
     <x-layout.app>
         <x-container>
-            <x-card title="Creating a link">
-                <x-form :route="route('links.create')" post id="create-form-link">
-                    <x-input name="link" id="link" value="{{ old('link') }}" placeholder="Link" />
-                    <x-input  name="name" id="name"  placeholder="Nome para o Link" value="{{ old('name')}}" />
+            <x-card title="Editing link :: {{ $link->id }}">
+                <x-form :route="route('links.edit', $link)" put id="edit-form-link">
+
+                    <x-input name="link" id="link" value="{{ old('link',$link->link) }}" placeholder="Link"/>
+                    <x-input name="name" id="name" placeholder="Nome para o Link"
+                             value="{{ old('name',$link->name)}}"/>
                 </x-form>
                 <x-slot:actions>
                     <x-a :href="route('dashboard')">Back to Dashboard</x-a>
-                    <x-button type="submit" form="create-form-link">Save</x-button>
+                    <x-button type="submit" form="edit-form-link">Edite</x-button>
                 </x-slot:actions>
             </x-card>
         </x-container>
     </x-layout.app>
-
 </x-layout.app>
